96387c46697f04f449843c7814982e09c6905735
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2021-04-21 00:08:47 +0100

    removed unneccesary using - probalby more to go still

---
+++ src/Prowlarr.Api.V1/Migration/MigrationController.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Prowlarr.Http;
+
+[V1ApiController]
+public class MigrationController : Controller 
+{
+
+}
