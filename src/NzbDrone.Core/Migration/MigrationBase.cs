b85886b386f2ef3acf1d53743007efdd48578874
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2021-04-21 00:11:32 +0100

    more scaffolding

---
+++ src/NzbDrone.Core/Migration/MigrationBase.cs
@@ -0,0 +1,3 @@
+namespace NzbDrone.Core.Migration
+{
+}
--- src/Prowlarr.Api.V1/Migration/MigrationController.cs
+++ src/Prowlarr.Api.V1/Migration/MigrationController.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Migration;
 using Prowlarr.Http;
 
 [V1ApiController]
 public class MigrationController : Controller 
 {
-
 }
